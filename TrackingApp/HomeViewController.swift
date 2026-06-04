7afda3f23ecf58f31e3dd7ec029aa4c3ce49b547
Mansour <theboom.kajas@gmail.com>
2026-06-04 20:36:50 +0200

    Changed the HomeViewController to a TabBarController

---
+++ TrackingApp/HomeViewController.swift
@@ -0,0 +1,41 @@
+//
+//  HomeViewController.swift
+//  TrackingApp
+//
+//  Created by Tardes on 4/6/26.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.isHidden = true
+        navigationItem.hidesBackButton = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.hidesBackButton = false
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
